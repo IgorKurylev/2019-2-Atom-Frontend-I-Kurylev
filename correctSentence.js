8704d9a253b1fdea33c5bc607d64507b71ff5d8f
Igor Kurylev <kurilevigor@mail.ru>
2019-10-03 20:56:55 +0300

    added hw1

--- correctSentence.js
+++ correctSentence.js
@@ -18,6 +18,9 @@ correctSentence("Greetings, friends.") == "Greetings, friends."
  */
 
 export default function correctSentence(text) {
-  // your solution goes here
-  return text;
+  if (text.endsWith('.')){
+    return text[0].toUpperCase() + text.slice(1)
+  }
+  return text[0].toUpperCase() + text.slice(1) + '.'
+  
 }

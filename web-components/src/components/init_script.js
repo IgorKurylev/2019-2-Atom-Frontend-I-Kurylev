ad4e63fc6cfa44bd3279562458f6702ae976f422
Igor Kurylev <kurilevigor@mail.ru>
2019-10-21 01:12:11 +0300

    fixed init_script for eslint

--- web-components/src/components/init_script.js
+++ web-components/src/components/init_script.js
@@ -1,21 +1,21 @@
 if (localStorage.getItem('dialogList') === null) {
-localStorage.setItem('dialogList', JSON.stringify([0, 1]));
+  localStorage.setItem('dialogList', JSON.stringify([0, 1]));
 
-localStorage.setItem('dialogID_0', JSON.stringify({
-  0: {
-    text: 'read message',
-    time: (new Date('2019-10-10')).getTime(),
-    owner: 8080,
-    status: 'read',
-  },
-}));
+  localStorage.setItem('dialogID_0', JSON.stringify({
+    0: {
+      text: 'read message',
+      time: (new Date('2019-10-10')).getTime(),
+      owner: 8080,
+      status: 'read',
+    },
+  }));
 
-localStorage.setItem('dialogID_1', JSON.stringify({
-  0: {
-    text: 'Message',
-    time: (new Date()).getTime(),
-    owner: 1,
-    status: 'new',
-  },
-}));
+  localStorage.setItem('dialogID_1', JSON.stringify({
+    0: {
+      text: 'Message',
+      time: (new Date()).getTime(),
+      owner: 1,
+      status: 'new',
+    },
+  }));
 }
